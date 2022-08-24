5e99093996ad2f12b2b2a8b6a9c158fa39fd252e
Emi <emi_chang@lhwlfw.com>
2022-08-24 15:48:54 +0800

    add leaflet

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import Map from '../components/Map';
+
+const DEFAULT_CENTER = [38.907132, -77.036546]
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+       <Map center={DEFAULT_CENTER} zoom={12}>
+          {({ TileLayer, Marker, Popup }) => (
+            <>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              />
+              <Marker position={DEFAULT_CENTER}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </>
+          )}
+        </Map>
   )
 }
